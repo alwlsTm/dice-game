6ab8c79fdaa998f5ef122d5ba7dea2e42b1d6c82
alwlsTm <mijine0716@naver.com>
2022-05-10 00:10:30 +0900

    220510 주사위 생성, 던지기/초기화 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import Button from './Button';
+import Dice from './Dice';
+
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 function App() {
+  const [num, setNum] = useState(1);
+
+  const handleRollClick = () => {
+    const nextNum = random(6);
+    setNum(nextNum);
+  };
+
+  const handleClearClick = () => {
+    setNum(1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <Button onClick={handleRollClick}>던지기</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
+      </div>
+      <Dice color='red' num={num} />
     </div>
   );
 }
