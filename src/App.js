18e8b1317594eb08f66d11b9ccbf83eb94d8776b
alwlsTm <mijine0716@naver.com>
2022-05-11 02:03:35 +0900

    220511 주사위 눈 총점, 기록 추가

--- src/App.js
+++ src/App.js
@@ -7,15 +7,23 @@ function random(n) {
 }
 
 function App() {
-  const [num, setNum] = useState(1);
+  const [num, setNum] = useState(1);    //주사위 눈 state (초기값 1)
+  const [sum, setSum] = useState(0);    //주사위 눈 총점 state
+  const [gameHistory, setGameHistory] = useState([]);   //주사위 눈 기록 state
 
+  //던지기
   const handleRollClick = () => {
-    const nextNum = random(6);
+    const nextNum = random(6);  //1 ~ 6
     setNum(nextNum);
+    setSum(sum + nextNum);  //새로운 주사위 눈 더하기
+    setGameHistory([...gameHistory, nextNum]);  //주사위 눈 기록 추가
   };
 
+  //처음부터(초기화)
   const handleClearClick = () => {
     setNum(1);
+    setSum(0);
+    setGameHistory([]);
   };
 
   return (
@@ -24,7 +32,14 @@ function App() {
         <Button onClick={handleRollClick}>던지기</Button>
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      <Dice color='red' num={num} />
+      <div>
+        <h2>나</h2>
+        <Dice color='blue' num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <h2>기록</h2>
+        {gameHistory.join(', ')}
+      </div>
     </div>
   );
 }
