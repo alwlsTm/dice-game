a50acf4feb755a4ec07785ca5677553303042ba4
alwlsTm <mijine0716@naver.com>
2022-05-12 00:21:17 +0900

    20220512 상대 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
+import Board from './Board';
 import Button from './Button';
-import Dice from './Dice';
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -11,12 +11,22 @@ function App() {
   const [sum, setSum] = useState(0);    //주사위 눈 총점 state
   const [gameHistory, setGameHistory] = useState([]);   //주사위 눈 기록 state
 
+  const [otherNum, setOtherNum] = useState(1);  //상대 주사위 눈 state
+  const [otherSum, setOtherSum] = useState(0);  //상대 주사위 눈 총점 state
+  const [otherGameHistory, setOtherGameHistory] = useState([]); //주사위 눈 기록 state
+
   //던지기
   const handleRollClick = () => {
     const nextNum = random(6);  //1 ~ 6
+    const nextOtherNum = random(6);
+
     setNum(nextNum);
     setSum(sum + nextNum);  //새로운 주사위 눈 더하기
     setGameHistory([...gameHistory, nextNum]);  //주사위 눈 기록 추가
+
+    setOtherNum(nextOtherNum);
+    setOtherSum(otherSum + nextOtherNum);
+    setOtherGameHistory([...otherGameHistory, nextOtherNum]);
   };
 
   //처음부터(초기화)
@@ -24,8 +34,12 @@ function App() {
     setNum(1);
     setSum(0);
     setGameHistory([]);
-  };
 
+    setOtherNum(1);
+    setOtherSum(0);
+    setOtherGameHistory([]);
+  };
+  
   return (
     <div>
       <div>
@@ -33,12 +47,8 @@ function App() {
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
       <div>
-        <h2>나</h2>
-        <Dice color='blue' num={num} />
-        <h2>총점</h2>
-        <p>{sum}</p>
-        <h2>기록</h2>
-        {gameHistory.join(', ')}
+        <Board name="나" color="blue" num={num} sum={sum} gameHistory={gameHistory} />
+        <Board name="상대" color="red" num={otherNum} sum={otherSum} gameHistory={otherGameHistory} />
       </div>
     </div>
   );
