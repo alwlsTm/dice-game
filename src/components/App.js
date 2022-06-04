6965d4f3efd03526de4885528ca3b143f9b56a4d
alwlsTm <mijine0716@naver.com>
2022-06-05 02:16:09 +0900

    20220605 주사위 게임 최종

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Board from './Board';
 import Button from './Button';
 import './App.css';
+import logo from '../IMGS/logo.png';
 
 function random(n) {
   return Math.ceil(Math.random() * n);
@@ -26,10 +27,14 @@ function App() {
   };
 
   return (
-    <div>
-      <div className='App'>
-        <Button className='App-button' color='blue' onClick={handleRollClick}>던지기</Button>
-        <Button className='App-button' color='red' onClick={handleClearClick}>처음부터</Button>
+    <div className='App'>
+      <div>
+        <img className='App-logo' src={logo} alt={logo}></img>
+        <h1 className='App-title'>주사위 게임</h1>
+        <div>
+          <Button className='App-button' color='blue' onClick={handleRollClick}>던지기</Button>
+          <Button className='App-button' color='red' onClick={handleClearClick}>처음부터</Button>
+        </div>
       </div>
       <div>
         <Board name="나" color="blue" gameHistory={myHistory} />
--- src/components/Board.js
+++ src/components/Board.js
@@ -1,11 +1,12 @@
 import Dice from './Dice';
+import './Board.css';
 
 function Board({ name, color, gameHistory }) {
   const num = gameHistory[gameHistory.length - 1] || 1; //현재 주사위의 눈(num)은 기록(gameHistory)의 마지막 값
   const sum = gameHistory.reduce((a, b) => a + b, 0);   //총점(sum)은 기록(gameHistory)의 합
 
   return (
-    <div>
+    <div className='Board'>
       <h2>{name}</h2>
       <Dice color={color} num={num} />
       <h2>총점</h2>
